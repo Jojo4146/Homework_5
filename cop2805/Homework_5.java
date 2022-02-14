935085a93ef03f241183a546a6589548a3f50c07
Johanna I. Scharbaai-Emilien <jse4146@me.com>
2022-02-14 12:26:05 -0500

    Calculator Layout

--- cop2805/Homework_5.java
+++ cop2805/Homework_5.java
@@ -1,15 +1,50 @@
 package cop2805;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-
-public class Homework_5 extends JFrame {
+public class Homework_5 {
 
     public static void main(String[] args) {
+        //Lambda that creates special thread to handle events with the GUI
+        // while calling constructGUI method
+        SwingUtilities.invokeLater(() -> constructGUI());
+    }
 
-        Frame frame = new Frame();
+    private static void constructGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame();
+        // Set the title
+        frame.setTitle("Simple Calculator");
+        // Shut down program on clicking corner X
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,250);
+
+        // Create a grid and set the columns and rows
+        frame.setLayout(new GridLayout(5, 2));
+
+        // First Row
+        frame.add(new JLabel("First Number:"));
+        frame.add(new JTextField());
+        // Second Row
+        frame.add(new JLabel("Second Number:"));
+        frame.add(new JTextField());
+        // Third Row
+        frame.add(new JLabel());            // Empty frame
+        String[] operators = { "Add", "Subtract", "Multiply", "Divide" };  // Start of JComboBox
+        JComboBox comboBox = new JComboBox(operators);
+        comboBox.setSelectedIndex(1);
+        System.out.println(comboBox.getSelectedItem());
+        frame.add(comboBox);
+        // Fourth Row
+        frame.add(new JLabel());            // Empty frame
+        frame.add(new JButton("Calculate"));
+        // Fifth Row
+        frame.add(new JLabel("Result:"));            // Empty frame
+
+        // Set width & height
+        frame.setSize(300,150);
+        // Set point relative to screen
+        frame.setBounds(800, 400, 300, 150);
         frame.setVisible(true);
     }
 }
