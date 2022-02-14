99393817e203c4a969e7f082cbfd5dc215fbdebd
Johanna I. Scharbaai-Emilien <jse4146@me.com>
2022-02-14 03:24:21 -0500

    Building GUI Panel

--- cop2805/Homework_5.java
+++ cop2805/Homework_5.java
@@ -1,8 +1,14 @@
 package cop2805;
 
+import javax.swing.JOptionPane;
+
 public class Homework_5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello y'all!");
+        String str1 = JOptionPane.showInputDialog("Enter first number");
+        String str2 = JOptionPane.showInputDialog("Enter Second number");
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
     }
 }
