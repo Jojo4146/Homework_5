ef01ad6499b1f6d2d1a50e34c93e68dc39bb9c3f
Johanna I. Scharbaai-Emilien <jse4146@me.com>
2022-02-14 14:17:03 -0500

    Event listeners

--- cop2805/Homework_5.java
+++ cop2805/Homework_5.java
@@ -8,36 +8,41 @@ public class Homework_5 {
     public static void main(String[] args) {
         //Lambda that creates special thread to handle events with the GUI
         // while calling constructGUI method
-        SwingUtilities.invokeLater(() -> constructGUI());
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                constructGUI();
+            }
+        });
     }
 
-    private static void constructGUI() {
+    private static void constructGUI(){
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame();
         // Set the title
         frame.setTitle("Simple Calculator");
         // Shut down program on clicking corner X
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         // Create a grid and set the columns and rows
         frame.setLayout(new GridLayout(5, 2));
-
         // First Row
         frame.add(new JLabel("First Number:"));
         frame.add(new JTextField());
+
         // Second Row
         frame.add(new JLabel("Second Number:"));
         frame.add(new JTextField());
+
         // Third Row
         frame.add(new JLabel());            // Empty frame
         String[] operators = { "Add", "Subtract", "Multiply", "Divide" };  // Start of JComboBox
-        JComboBox comboBox = new JComboBox(operators);
-        comboBox.setSelectedIndex(1);
-        System.out.println(comboBox.getSelectedItem());
-        frame.add(comboBox);
+        JComboBox cb = new JComboBox(operators);
+        cb.setSelectedIndex(1);
+        System.out.println(cb.getSelectedItem());
+        frame.add(cb);
         // Fourth Row
         frame.add(new JLabel());            // Empty frame
         frame.add(new JButton("Calculate"));
+
         // Fifth Row
         frame.add(new JLabel("Result:"));            // Empty frame
 
